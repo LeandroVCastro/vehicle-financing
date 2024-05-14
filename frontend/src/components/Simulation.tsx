4c8363845faac232af9685e53fab8d8c9532326b
Leandro Castro <leandro.lvc@hotmail.com>
2024-05-14 15:20:05 -0300

    feat: print message error simulation

--- frontend/src/components/Simulation.tsx
+++ frontend/src/components/Simulation.tsx
@@ -8,7 +8,7 @@ import ContactComponent from "./Contact";
 import PricingBoxSimulationComponent from "./PricingBoxSimulation";
 
 export default function SimulationComponent() {
-  const { selectedVehicle, simulated } = useContext(
+  const { selectedVehicle, simulated, entryValue } = useContext(
     VehiclesContext
   ) as VehiclesContextType;
 
@@ -16,19 +16,27 @@ export default function SimulationComponent() {
     <>
       {simulated && selectedVehicle && (
         <div className={style.box} style={{ padding: "25px" }}>
+          {}
           <h4 className={`${style.subTitle}  ${style.lineBottom}`}>
             Valores simulados para você
           </h4>
+          {(entryValue ?? 0) < selectedVehicle.price ? (
+            <>
+              <div className={style.princingContainer}>
+                <PricingBoxSimulationComponent times={6} ipvaFree={true} />
+                <div className={style.smallPrincigBoxContainer}>
+                  <PricingBoxSimulationComponent times={12} />
+                  <PricingBoxSimulationComponent times={48} />
+                </div>
+              </div>
 
-          <div className={style.princingContainer}>
-            <PricingBoxSimulationComponent times={6} ipvaFree={true} />
-            <div className={style.smallPrincigBoxContainer}>
-              <PricingBoxSimulationComponent times={12} />
-              <PricingBoxSimulationComponent times={48} />
-            </div>
-          </div>
-
-          <ContactComponent />
+              <ContactComponent />
+            </>
+          ) : (
+            <h3 className={style.errorSimulation}>
+              O valor da entrada precisa ser menor do que o valor do veículo
+            </h3>
+          )}
         </div>
       )}
     </>
